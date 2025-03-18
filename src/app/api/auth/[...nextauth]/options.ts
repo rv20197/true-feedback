54e0d79714541b6c1ea4f3bafb8d3d9a0831a80e
Vatsal <rv20197@gmail.com>
2025-03-18 09:38:19 +0530

    Setup JWT token and Session for Sign in

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -11,7 +11,7 @@ export const authOptions: NextAuthOptions = NextAuth({
             username: { label: "Username", type: "text", placeholder: "jsmith" },
             password: { label: "Password", type: "password" }
         },
-        async authorize(credentials):Promise<void> {
+        async authorize(credentials) {
             await dbConnect();
 
             try{
@@ -28,7 +28,7 @@ export const authOptions: NextAuthOptions = NextAuth({
                     throw new Error("Please verify your account before login")
                 }
 
-                const isPasswordCorrect:Promise<boolean> = await bcrypt.compare(credentials.password,user.password);
+                const isPasswordCorrect = await bcrypt.compare(credentials.password as string,user.password);
 
                 if(!isPasswordCorrect){
                     throw new Error("Invalid credentials");
@@ -42,5 +42,29 @@ export const authOptions: NextAuthOptions = NextAuth({
     })],
     pages:{
         signIn: "/sign-in"
+    },
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        async jwt({token,user}){
+            if(user){
+                token._id = user.id?.toString();
+                token.isVerified = user.isVerified;
+                token.isAcceptingMessages = user.isAcceptingMessages;
+                token.username = user.username;
+            }
+            return token;
+        },
+        async session({session,token}){
+            if(token){
+                session.user._id = token._id;
+                session.user.isVerified = token.isVerified;
+                session.user.isAcceptingMessages = token.isAcceptingMessages;
+                session.user.username = token.username;
+            }
+            return session;
+        }
     }
 })
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,22 @@
+import 'next-auth';
+
+declare module 'next-auth' {
+    interface User{
+        _id?: string;
+        isVerified?: boolean;
+        isAcceptingMessages?: boolean;
+        username?: string;
+    }
+    interface Session{
+        user: User & DefaultSession['user'];
+    }
+}
+
+declare module 'next-auth/jwt' {
+    interface JWT{
+        _id?: string;
+        isVerified?: boolean;
+        isAcceptingMessages?: boolean;
+        username?: string;
+    }
+}

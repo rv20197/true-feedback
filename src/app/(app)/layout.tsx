12916d1fd5d18a365e5ef272b5279d1f1828aac3
Vatsal <rv20197@gmail.com>
2025-03-24 07:31:10 +0530

    Added Navbar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,3 +1,5 @@
+import Navbar from "@/components/Navbar/Navbar";
+
 interface RootLayoutProps {
     children: React.ReactNode;
 }
@@ -5,6 +7,7 @@ interface RootLayoutProps {
 export default async function RootLayout({children}: RootLayoutProps) {
     return (
         <div className="flex flex-col min-h-screen">
+            <Navbar/>
             {children}
         </div>
     );
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,45 @@
+'use client';
+import Link from "next/link";
+import {signOut, useSession} from "next-auth/react";
+import {User} from 'next-auth';
+import {Button} from "@/components/ui/button";
+import {Fragment} from "react";
+
+/**
+ * The Navbar component is a Next.js component that renders a navigation bar
+ * with a sign in or sign out button depending on the user's authentication
+ * state.
+ *
+ * @returns A JSX element representing the Navbar component.
+ */
+const Navbar = () => {
+    /**
+     * The user's session, if any.
+     */
+    const {data: session} = useSession();
+    /**
+     * The user, if any.
+     */
+    const user: User = session?.user as User;
+
+    return (
+        <nav className={'p-4 md:p-6 flex justify-between shadow-md'}>
+            <div className={'container mx-auto flex justify-between items-center gap-4 flex-col md:flex-row'}>
+                <Link href={"#"} className={'text-xl font-bold md:mb-0'}>Mystery Message</Link>
+                {session ? (
+                    <Fragment>
+                        <span className={'mr-4'}>Welcome , {user?.name || user?.email}</span>
+                        <Button className='w-full md:w-auto' onClick={() => signOut()}>Sign Out</Button>
+                    </Fragment>
+                ) : (
+                    <Link href={'/sign-in'}>
+                        <Button className='w-full md:w-auto'>
+                            Sign In
+                        </Button>
+                    </Link>
+                )}
+            </div>
+        </nav>
+    )
+}
+export default Navbar
